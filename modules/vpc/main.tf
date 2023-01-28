1e3c0ca53c254839a188a10959b2e8a4dea85219
aws-ref-architect <123782877+aws-ref-architect@users.noreply.github.com>
2023-01-27 19:26:59 -0800

    Fix

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -9,28 +9,31 @@ variable "cidr" {
 }
 
 variable "availability_zones" {
-  description = "List of availability zones for VPC."
+  description = "List of availability zones for the VPC."
   type = "list"
 }
 
 variable "subnets" {
-  description = "List of subnet IDs for VPC."
+  description = "List of subnet IDs for the VPC."
   type = "list"
 }
 
 variable "environment" {
-  description = "Environment name/tag for VPC. Defaults are test, development, staging, and production."
+  description = "Environment name/tag for the VPC. Defaults are test, development, staging, and production."
   default = "test"
   type = "string"
 }
 
-resource_block "aws_vpc" "${var.name}" {
-  cidr_block = "${var.cidr}"
-  enable_dns_support = true
-  enable_dns_hostnames = true
+module "vpc" {
+  source = "./"
 
-  tags {
-    Name        = "${var.name}"
-    Environment = "${var.environment}"
-  }
+  resource_block "aws_vpc" "${var.name}" {
+    cidr_block = "${var.cidr}"
+    enable_dns_support = true
+    enable_dns_hostnames = true
+
+    tags {
+      Name        = "${var.name}"
+      Environment = "${var.environment}"
+    }
 }
