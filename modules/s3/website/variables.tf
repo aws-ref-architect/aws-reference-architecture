be3150ab351d5d9f80fb08f0914746261e9074ea
aws-ref-architect <123782877+aws-ref-architect@users.noreply.github.com>
2023-05-01 16:55:50 +0000

    Add s3 website environments

--- modules/s3/website/variables.tf
+++ modules/s3/website/variables.tf
@@ -3,6 +3,11 @@ variable "bucket_name" {
   type        = string
 }
 
+variable "environment" {
+  description = "Specify one of: production, staging, development, or development_0000001-development_1000000"
+  type = string
+}
+
 variable "tags" {
   description = "Tags."
   type        = map(string)
