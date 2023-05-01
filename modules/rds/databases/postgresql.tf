2fab10788282dab1b3e7428488a687174cce9218
aws-ref-architect <123782877+aws-ref-architect@users.noreply.github.com>
2023-05-01 00:49:14 +0000

    Add postgresql RDS database definition

--- modules/rds/databases/postgresql.tf
+++ modules/rds/databases/postgresql.tf
@@ -2,20 +2,33 @@ resource "aws_kms_key" "database_key" {
   description = "KMS key for RDS database."
 }
 
+resource "security_group" "postgresql_rds" {
+  name        = "private-to-database"
+  description = "Allow inbound traffic from private instances only."
+
+  ingress {
+    description = "HTTP"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = var.private_cidr
+  }
+}
+
 resource "aws_db_instance" "rds_postgresql" {
-  identifier                            = "${environment}-postgresql"
-  name                                  = "${environment}-postgresql"
-  instance_class                        = "db.t3.micro"
-  allocated_storage                     = 10
-  max_allocated_storage                 = 100
+  identifier                            = "${var.environment}-postgresql"
+  name                                  = "${var.environment}-${var.postgresql_database_name}"
+  instance_class                        = var.instance_class
+  allocated_storage                     = var.min_allocated_storage
+  max_allocated_storage                 = var.max_allocated_storage
   engine                                = "postgres"
-  engine_version                        = "15.2"
+  engine_version                        = var.postgresql_engine_version
   backup_retention_period               = 0
   delete_automated_backups              = false
   deletion_protection                   = true
   skip_final_snapshot                   = false
   publicly_accessible                   = false
-  vpc_security_group_ids                = [aws_security_group.postgresql.id]
+  vpc_security_group_ids                = [aws_security_group.postgresql_rds.id]
   manage_master_user_password           = true
   master_user_secret_kms_key_id         = aws_kms_key.database_key.key_id
   db_name                               = "postgres"
@@ -33,70 +46,4 @@ resource "aws_db_instance" "rds_postgresql" {
   performance_insights_enabled          = true
   performance_insights_retention_period = 7
   port                                  = 5432
-}
-
-resource "aws_db_instance" "rds_mysql" {
-  identifier                            = "${environment}-mysql"
-  name                                  = "${environment}-mysql"
-  instance_class                        = "db.t3.micro"
-  allocated_storage                     = 10
-  max_allocated_storage                 = 100
-  engine                                = "mysql"
-  engine_version                        = "8.0.32"
-  backup_retention_period               = 0
-  delete_automated_backups              = false
-  deletion_protection                   = true
-  skip_final_snapshot                   = false
-  publicly_accessible                   = false
-  vpc_security_group_ids                = [aws_security_group.mysql.id]
-  manage_master_user_password           = true
-  master_user_secret_kms_key_id         = aws_kms_key.database_key.key_id
-  db_name                               = "mysql"
-  username                              = "mysql"
-  storage_encrypted                     = "true"
-  backup_window                         = "10:00-11:00"
-  maintenance_window                    = "Mon:11:00-Mon:12:00"
-  auto_minor_version_upgrade            = true
-  allow_major_version_upgrade           = true
-  apply_immediately                     = false
-  multi_az                              = true
-  availability_zone                     = "us-east-1a"
-  enabled_cloudwatch_logs_exports       = ["audit", "error", "general", "slowquery"]
-  monitoring_interval                   = 1
-  performance_insights_enabled          = true
-  performance_insights_retention_period = 7
-  port                                  = 3306
-}
-
-resource "aws_db_instance" "rds_mariadb" {
-  identifier                            = "${environment}-mariadb"
-  name                                  = "${environment}-mariadb"
-  instance_class                        = "db.t3.micro"
-  allocated_storage                     = 10
-  max_allocated_storage                 = 100
-  engine                                = "mariadb"
-  engine_version                        = "10.6"
-  backup_retention_period               = 0
-  delete_automated_backups              = false
-  deletion_protection                   = true
-  skip_final_snapshot                   = false
-  publicly_accessible                   = false
-  vpc_security_group_ids                = [aws_security_group.mariadb.id]
-  manage_master_user_password           = true
-  master_user_secret_kms_key_id         = aws_kms_key.database_key.key_id
-  db_name                               = "mariadb"
-  username                              = "mariadb"
-  storage_encrypted                     = "true"
-  backup_window                         = "10:00-11:00"
-  maintenance_window                    = "Mon:11:00-Mon:12:00"
-  auto_minor_version_upgrade            = true
-  allow_major_version_upgrade           = true
-  apply_immediately                     = false
-  multi_az                              = true
-  availability_zone                     = "us-east-1a"
-  enabled_cloudwatch_logs_exports       = ["audit", "error", "general", "slowquery"]
-  monitoring_interval                   = 1
-  performance_insights_enabled          = true
-  performance_insights_retention_period = 7
-  port                                  = 3306
 }
---
+++ modules/rds/variables.tf
@@ -0,0 +1,38 @@
+// All environments.
+variable "availability_zones" {
+  description = "Availability zones used by project."
+  type        = list(string)
+  default     = ["us-east-2a", "us-east-2b", "us-east-2c"]
+}
+
+variable "environment" {
+  description = "Specify one of: production, staging, development, or development_0000001-development_1000000"
+  type = string
+}
+
+variable "min_allocated_storage" {
+  description = "Minimum storage allocation in GBs."
+  type = number
+  default = 10
+}
+
+variable "max_allocated_storage" {
+  description = "Minimum storage allocation in GBs."
+  type = number
+  default = 100
+}
+
+variable "postgresql_database_name" {
+  type = string
+  default = "postgres"
+}
+
+variable "postgresql_engine_version" {
+  type = string
+  default = 15.2
+}
+
+variable "instance_class" {
+  type = "string"
+  default = "db.t3.micro"
+}
