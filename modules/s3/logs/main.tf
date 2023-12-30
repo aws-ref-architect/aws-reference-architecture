fe249637f6d301801dc4b24f9e7a91461f4bce16
aws-ref-architect <123782877+aws-ref-architect@users.noreply.github.com>
2023-12-30 02:37:24 +0000

    Add s3 bucket for logs

--- modules/s3/logs/main.tf
+++ modules/s3/logs/main.tf
@@ -1,24 +1,66 @@
-resource "aws_s3_bucket" "global_logs" {
-  bucket = "global-logs"
+resource "aws_s3_bucket" "global-logs" {
+  bucket = "${var.company_s3_bucket_prefix}-global-logs"
 }
 
-resource "aws_s3_bucket_acl" "global_logs" {
-  bucket = aws_s3_bucket.global_logs.id
+resource "aws_s3_bucket_ownership_controls" "oc-global-logs" {
+  bucket = aws_s3_bucket.global-logs.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "oc-global-logs" {
+  bucket = aws_s3_bucket.global-logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_acl" "global-logs-acl"  {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.oc-global-logs,
+    aws_s3_bucket_public_access_block.oc-global-logs,
+  ]
+
+  bucket = aws_s3_bucket.global-logs.id
   acl    = "private"
 }
 
-resource "aws_s3_bucket" "logs_bucket" {
-  bucket = "logs-bucket"
+resource "aws_s3_bucket" "logs-bucket" {
+  bucket = "${var.company_s3_bucket_prefix}-logs-bucket"
 }
 
-resource "aws_s3_bucket_acl" "logs_bucket_acl" {
-  bucket = aws_s3_bucket.logs_bucket.id
+resource "aws_s3_bucket_ownership_controls" "oc-logs-bucket" {
+  bucket = aws_s3_bucket.logs-bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "oc-logs-bucket" {
+  bucket = aws_s3_bucket.logs-bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_acl" "logs-bucket-acl"  {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.oc-logs-bucket,
+    aws_s3_bucket_public_access_block.oc-logs-bucket,
+  ]
+
+  bucket = aws_s3_bucket.logs-bucket.id
   acl    = "log-delivery-write"
 }
 
 resource "aws_s3_bucket_logging" "logging" {
-  bucket = aws_s3_bucket.global_logs.id
+  bucket = aws_s3_bucket.global-logs.id
 
-  target_bucket = aws_s3_bucket.logs_bucket.id
+  target_bucket = aws_s3_bucket.logs-bucket.id
   target_prefix = "log/"
 }
--- modules/s3/terraform_state/main.tf
+++ modules/s3/terraform_state/main.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "s3_terraform_state" {
-  bucket = "terraform-state-backend"
+resource "aws_s3_bucket" "s3-terraform-state" {
+  bucket = "${var.company_s3_bucket_prefix}-terraform-state"
   object_lock_enabled = true
   tags = {
     "Environment": "global"
@@ -7,14 +7,14 @@ resource "aws_s3_bucket" "s3_terraform_state" {
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
-  bucket = aws_s3_bucket.s3_terraform_state.id
+  bucket = aws_s3_bucket.s3-terraform-state.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
-  bucket = aws_s3_bucket.s3_terraform_state.id
+  bucket = aws_s3_bucket.s3-terraform-state.id
 
   rule {
     apply_server_side_encryption_by_default {
@@ -24,5 +24,5 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_e
 }
 
 resource "aws_s3_bucket_object_lock_configuration" "terraform_state_object_lock" {
-  bucket = aws_s3_bucket.s3_terraform_state.id
+  bucket = aws_s3_bucket.s3-terraform-state.id
 }
